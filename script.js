1bf0d5d63005750e1e795967489b48c198cfe3bc
Akram Hafaiedh <Hfaiedhakram@gmail.com>
2021-12-31 14:29:01 +0100

    finished Homepage

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
-
-function navToggle() {
-  btn.classList.toggle('open')
-  nav.classList.toggle('hidden')
-  document.body.classList.toggle('no-scroll')
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+function navToggle(){
+    // console.log(1);
+    btn.classList.toggle('open');
+    nav.classList.toggle('hidden');
+    document.body.classList.toggle('no-scroll');
 }
-
-btn.addEventListener('click', navToggle)
+btn.addEventListener('click', navToggle);
